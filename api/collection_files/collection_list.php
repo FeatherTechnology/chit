98b20a64be319d3cb198434698fa90ac58ac5940
akshaya <akshayaashok331@gmail.com>
2024-10-21 10:23:54 +0530

    collection
    updated

--- api/collection_files/collection_list.php
+++ api/collection_files/collection_list.php
@@ -1,7 +1,6 @@
 <?php
 @session_start();
 require '../../ajaxconfig.php';
-
 $currentMonth = date('m');
 $currentYear = date('Y');
 $user_id = $_SESSION['user_id'];
@@ -35,7 +34,7 @@ $query = "SELECT
         SELECT
             GROUP_CONCAT(sc.occupation SEPARATOR ', ')
         FROM 
-            SOURCE sc
+            source sc
         WHERE
             sc.cus_id = cc.cus_id
     ) AS occupations,
@@ -47,25 +46,18 @@ $query = "SELECT
     ad.auction_month
 FROM
     auction_details ad
-LEFT JOIN group_cus_mapping gcm ON
-    ad.group_id = gcm.grp_creation_id
-LEFT JOIN customer_creation cc ON
-    gcm.cus_id = cc.id
-LEFT JOIN place pl ON
-    cc.place = pl.id
-LEFT JOIN group_creation gc ON
-    ad.group_id = gc.grp_id
-JOIN users us ON
-    FIND_IN_SET(gc.branch, us.branch)
+LEFT JOIN group_cus_mapping gcm ON ad.group_id = gcm.grp_creation_id
+LEFT JOIN customer_creation cc ON gcm.cus_id = cc.id
+LEFT JOIN place pl ON cc.place = pl.id
+LEFT JOIN group_creation gc ON ad.group_id = gc.grp_id
+JOIN users us ON FIND_IN_SET(gc.branch, us.branch)
 WHERE
-    gc.status=3
+    gc.status = 3
     AND YEAR(ad.date) = '$currentYear'
-    AND MONTH(ad.date) ='$currentMonth'
-"; // Removed the subquery checking for 'Paid' status
-
+    AND MONTH(ad.date) = '$currentMonth'";
 if (isset($_POST['search']) && $_POST['search'] != "") {
     $search = $_POST['search'];
-    $query .= " AND ( cc.cus_id LIKE '%" . $search . "%'
+    $query .= " AND (cc.cus_id LIKE '%" . $search . "%'
                       OR CONCAT(cc.first_name, ' ', cc.last_name) LIKE '%" . $search . "%'
                       OR cc.mobile1 LIKE '%" . $search . "%'
                       OR pl.place LIKE '%" . $search . "%'
@@ -92,46 +84,41 @@ $number_filter_row = $statement->rowCount();
 
 $statement = $pdo->prepare($query . $query1);
 $statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// Now filter out those that are already 'Paid'
-$filtered_results = [];
-foreach ($result as $row) {
-    $status = $collectionSts->updateCollectStatus($row['cus_id'], $row['id']);
-    if ($status !== 'Paid') {
-        $filtered_results[] = $row; // Only include those not 'Paid'
-    }
-}
+$result = $statement->fetchAll();
 
 $sno = isset($_POST['start']) ? $_POST['start'] + 1 : 1;
 $data = [];
-foreach ($filtered_results as $row) {
-    $sub_array = array();
-    $sub_array[] = $sno++;
-    $sub_array[] = isset($row['cus_id']) ? $row['cus_id'] : '';
-    $sub_array[] = isset($row['cus_name']) ? $row['cus_name'] : '';
-    $sub_array[] = isset($row['mobile1']) ? $row['mobile1'] : '';
-    $sub_array[] = isset($row['place']) ? $row['place'] : '';
-    $sub_array[] = isset($row['occupations']) ? $row['occupations'] : '';
-
-    // Fetch status using the correct method call
+foreach ($result as $row) {
     $status = $collectionSts->updateCollectStatus($row['cus_id'], $row['id']);
     $grace_status = $graceperiodSts->updateGraceStatus($row['cus_id'], $row['id']);
-    $sub_array[] = $status;
-      
-    if ($status === "Paid") {
-        $status_color = 'green'; // Payment is made
-    } elseif ($grace_status === 'orange') {
-        $status_color = 'orange'; // Payment is due but not yet
-    } elseif ($grace_status === 'red') {
-        $status_color = 'red'; // Missed payment after grace period
-    }
-    $sub_array[] = "<span style='display: inline-block; width: 20px; height: 20px; border-radius: 4px; background-color: $status_color;'></span>";
 
-    $action = "<button class='btn btn-primary collectionListBtn' value='" . $row['id'] . "'>&nbsp;View</button>";
-    $sub_array[] = $action;
-    $data[] = $sub_array;
+    // Exclude customers with a 'Paid' status
+    if ($status !== "Paid") {
+        $sub_array = array();
+        $sub_array[] = $sno++; // Increment the serial number only for 'Payable' customers
+        $sub_array[] = isset($row['cus_id']) ? $row['cus_id'] : '';
+        $sub_array[] = isset($row['cus_name']) ? $row['cus_name'] : '';
+        $sub_array[] = isset($row['mobile1']) ? $row['mobile1'] : '';
+        $sub_array[] = isset($row['place']) ? $row['place'] : '';
+        $sub_array[] = isset($row['occupations']) ? $row['occupations'] : '';
+        $sub_array[] = $status;
+        // Determine the status color
+        if ($grace_status === 'orange') {
+            $status_color = 'orange';
+        } elseif ($grace_status === 'red') {
+            $status_color = 'red';
+        } else {
+            $status_color = 'grey'; // Default color for 'Payable'
+        }
+
+        $sub_array[] = "<span style='display: inline-block; width: 20px; height: 20px; border-radius: 4px; background-color: $status_color;'></span>";
+
+        $action = "<button class='btn btn-primary collectionListBtn' value='" . $row['id'] . "'>&nbsp;View</button>";
+        $sub_array[] = $action;
+        $data[] = $sub_array;
+    }
 }
+
 
 function count_all_data($pdo)
 {
@@ -144,7 +131,7 @@ function count_all_data($pdo)
 $output = array(
     'draw' => isset($_POST['draw']) ? intval($_POST['draw']) : 0,
     'recordsTotal' => count_all_data($pdo),
-    'recordsFiltered' => count($filtered_results), // Use filtered results count
+    'recordsFiltered' => $number_filter_row,
     'data' => $data
 );
 
