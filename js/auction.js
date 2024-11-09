f2e6e8074599b01cfe0a658479d7761e2d6ff49a
akshaya <akshayaashok331@gmail.com>
2024-11-09 13:04:35 +0530

    Auction
    Update customer name with place and customer Id in Auction list

--- js/auction.js
+++ js/auction.js
@@ -1001,7 +1001,7 @@ function getCusName(groupId, auction_month) {
         response.forEach(function (val) {
             items.push({
                 value: val.id,
-                label: val.cus_name + ' - ' + val.place,
+                label: val.cus_name + ' - ' + val.place +' - ' + val.cus_id,
                 selected: false
             });
         });
