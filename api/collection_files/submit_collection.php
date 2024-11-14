2da138463fd25c00a9419b9bd05d8f2c166374cd
akshaya <akshayaashok331@gmail.com>
2024-11-14 12:00:52 +0530

    Collection
    Print Collection ID Corrections - Completed

--- api/collection_files/submit_collection.php
+++ api/collection_files/submit_collection.php
@@ -26,13 +26,13 @@ if ($collection_amount >= $payable) {
 /// Insert the collection record
 $qry = $pdo->query("INSERT INTO collection (cus_mapping_id, auction_id, group_id, cus_id, auction_month, chit_value, chit_amount, pending, payable, coll_status, collection_date, coll_mode, transaction_id, bank_id, collection_amount, insert_login_id, created_on) 
 VALUES ('$cus_mapping_id', '$auction_id', '$group_id', '$cus_id', '$auction_month', '$chit_value', '$chit_amount', '$pending', '$payable', '$status', '$collection_date " . date(' H:i:s') . "', '$coll_mode', '$transaction_id', '$bank_name', '$collection_amount', '$user_id', CURRENT_TIMESTAMP())");
-$update_query = $pdo->query("UPDATE group_cus_mapping SET 
-coll_status = '$status'
-WHERE id = '$cus_mapping_id'");
+
 if ($qry) {
     // Fetch the last inserted collection ID
     $coll_id = $pdo->lastInsertId();
-
+    $update_query = $pdo->query("UPDATE group_cus_mapping SET 
+    coll_status = '$status'
+    WHERE id = '$cus_mapping_id'");
     // Fetch the group information
     $groupInfo = $pdo->query("SELECT start_month, end_month FROM group_creation WHERE grp_id = '$group_id' AND end_month <= DATE_FORMAT(CURDATE(), '%Y-%m')")->fetch(PDO::FETCH_ASSOC);
 
